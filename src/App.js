144be8726216ff3f8309646b5f303228b16d8c3f
manuelpapa <manulito@de-vio.com>
2020-08-12 12:30:29 +0200

    Add css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./App.css";
+import List from "./components/list";
 
 function App() {
-  return <div>Pokedex</div>;
+  return (
+    <div className="app">
+      <header>
+        <span className="title">Pokemon</span> <input placeholder="🔎 Search" />
+      </header>
+      <main>
+        <nav>
+          <List>
+            Tescht
+            {/* <ListItem>
+              <ListItemIcon src="">
+                <ListItemText primary="">
+                  </ListItemText>
+                </ListItem> */}
+          </List>
+        </nav>
+      </main>
+      <footer>Tabs</footer>
+    </div>
+  );
 }
 
 export default App;
