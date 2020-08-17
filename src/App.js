772f5c6894129393ad35276d9aaa627317162252
manuelpapa <manulito@de-vio.com>
2020-08-17 12:12:58 +0200

    Add dynamic url via useParams

--- src/App.js
+++ src/App.js
@@ -1,65 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import List from "./components/List";
-import ListItem from "./components/ListItem";
-import ListItemText from "./components/ListItemText";
-import ListItemIcon from "./components/ListItemIcon";
-import { fetchPokemons } from "./api/pokemons";
-import LoadingScreen from "./components/LoadingScreen";
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Items from "./pages/Items";
+import Pokemon from "./pages/Pokemon";
+import Pokemons from "./pages/Pokemons";
 
 function App() {
-  const [pokemons, setPokemons] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      const allPokemons = await fetchPokemons();
-      setPokemons(allPokemons);
-      setIsLoading(false);
-    }
-    fetchData();
-  }, []);
-
-  if (isLoading || pokemons === null) {
-    return <LoadingScreen />;
-  }
-
-  const filteredPokemons = pokemons.filter((pokemon) => {
-    return pokemon.name.toLowerCase().startsWith(query.toLowerCase());
-  });
-
   return (
     <Router>
       <div className="app">
-        <header>
-          Pokedex{" "}
-          <input
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-            placeholder="Enter name"
-          />
-        </header>
-        <main className="colorful-border">
-          <List>
-            {filteredPokemons.map((pokemon) => (
-              <ListItem key={pokemon.id} href={pokemon.link}>
-                <ListItemIcon
-                  src={pokemon.imgSrc}
-                  alt={`Picture of ${pokemon.name}`}
-                />
-                <ListItemText
-                  primary={pokemon.name}
-                  secondary={`#${pokemon.id}`}
-                />
-              </ListItem>
-            ))}
-          </List>
-        </main>
-        <footer>Footer</footer>
+        <Switch>
+          <Route path="/pokemons/:name">
+            <Pokemon />
+          </Route>
+          <Route path="/pokemons">
+            <Pokemons />
+          </Route>
+          <Route path="/items">
+            <Items />
+          </Route>
+          <Route path="/">
+            <Redirect to="/pokemons" />
+          </Route>
+        </Switch>
+        <footer>
+          <Link to="/pokemons">Pokemons</Link>
+          <Link to="/items">Items</Link>
+        </footer>
       </div>
     </Router>
   );
